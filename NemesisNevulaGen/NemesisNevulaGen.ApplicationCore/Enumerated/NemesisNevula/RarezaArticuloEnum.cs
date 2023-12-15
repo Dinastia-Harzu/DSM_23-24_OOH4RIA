791b680faff582b1cd1056c725daab5f735084ba
Dinastía Harzu αγρ <111463709+Dinastia-Harzu@users.noreply.github.com>
2023-12-15 21:00:36 +0100

    Delete NemesisNevulaGen directory

--- NemesisNevulaGen/NemesisNevulaGen.ApplicationCore/Enumerated/NemesisNevula/RarezaArticuloEnum.cs
+++
@@ -1,7 +0,0 @@
-
-using System;
-
-namespace NemesisNevulaGen.ApplicationCore.Enumerated.NemesisNevula
-{
-public enum RarezaArticuloEnum { comun=1, raro=2, legendario=3, premium=4 };
-}
--- NemesisNevulaGen/NemesisNevulaGen.ApplicationCore/Enumerated/NemesisNevula/TipoPagoEnum.cs
+++
@@ -1,7 +0,0 @@
-
-using System;
-
-namespace NemesisNevulaGen.ApplicationCore.Enumerated.NemesisNevula
-{
-public enum TipoPagoEnum { paypal=1, tarjeta=2, cartera=3 };
-}
