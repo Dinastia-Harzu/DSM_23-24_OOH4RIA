485bc4a15b5ce84bb8fe039c62383da7bed04da2
Moonspoppy <paulario02@gmail.com>
2023-12-21 03:22:51 +0100

    Pequeños cambios

--- NemesisNevulaGen/NemesisNevulaGen.ApplicationCore/CP/manual/UsuarioPremiumCP_crearUsuarioPremium.cs
+++ NemesisNevulaGen/NemesisNevulaGen.ApplicationCore/CP/manual/UsuarioPremiumCP_crearUsuarioPremium.cs
@@ -16,31 +16,37 @@ using NemesisNevulaGen.ApplicationCore.CEN.NemesisNevula;
 
 namespace NemesisNevulaGen.ApplicationCore.CP.NemesisNevula
 {
-public partial class UsuarioPremiumCP : GenericBasicCP
-{
-public NemesisNevulaGen.ApplicationCore.EN.NemesisNevula.UsuarioPremiumEN CrearUsuarioPremium (string p_nombre, string p_correo, bool p_conGoogle, string p_foto_perfil, int p_puntosNevula, float p_cartera, String p_pass)
-{
-        /*PROTECTED REGION ID(NemesisNevulaGen.ApplicationCore.CP.NemesisNevula_UsuarioPremium_crearUsuarioPremium) ENABLED START*/
-
-        UsuarioPremiumCEN usuarioPremiumCEN = null;
-        UsuarioCEN usuarioCEN = null;
-
-        NemesisNevulaGen.ApplicationCore.EN.NemesisNevula.UsuarioPremiumEN result = null;
-
-
-        try
+    public partial class UsuarioPremiumCP : GenericBasicCP
+    {
+        public NemesisNevulaGen.ApplicationCore.EN.NemesisNevula.UsuarioPremiumEN CrearUsuarioPremium(string p_nombre, string p_correo, bool p_conGoogle, string p_foto_perfil, int p_puntosNevula, float p_cartera, String p_pass)
         {
-                CPSession.SessionInitializeTransaction ();
-                usuarioPremiumCEN = new  UsuarioPremiumCEN (CPSession.UnitRepo.UsuarioPremiumRepository);
-                usuarioCEN = new UsuarioCEN (CPSession.UnitRepo.UsuarioRepository);
+            /*PROTECTED REGION ID(NemesisNevulaGen.ApplicationCore.CP.NemesisNevula_UsuarioPremium_crearUsuarioPremium) ENABLED START*/
 
-                IList<UsuarioEN> usuario = usuarioCEN.DamePorNombre (p_nombre);
+            UsuarioPremiumCEN usuarioPremiumCEN = null;
+            UsuarioCEN usuarioCEN = null;
+
+            NemesisNevulaGen.ApplicationCore.EN.NemesisNevula.UsuarioPremiumEN result = null;
+
+
+            try
+            {
+                CPSession.SessionInitializeTransaction();
+                usuarioPremiumCEN = new UsuarioPremiumCEN(CPSession.UnitRepo.UsuarioPremiumRepository);
+                usuarioCEN = new UsuarioCEN(CPSession.UnitRepo.UsuarioRepository);
+
+                IList<UsuarioEN> usuario = usuarioCEN.DamePorNombre(p_nombre);
+
+                if (usuario[0].Cartera < 10)
+                {
+                    return null;
+                }
+                else { usuario[0].Cartera = usuario[0].Cartera - 10; }
 
                 int oid;
                 //Initialized UsuarioPremiumEN
                 UsuarioPremiumEN usuarioPremiumEN;
 
-                usuarioPremiumEN = new UsuarioPremiumEN ();
+                usuarioPremiumEN = new UsuarioPremiumEN();
                 usuarioPremiumEN.Nombre = p_nombre;
 
                 usuarioPremiumEN.Correo = p_correo;
@@ -55,35 +61,36 @@ public NemesisNevulaGen.ApplicationCore.EN.NemesisNevula.UsuarioPremiumEN CrearU
 
                 usuarioPremiumEN.Pass = Utils.Util.GetEncondeMD5(p_pass);
 
-                IList<int> aux = new List<int> ();
+                IList<int> aux = new List<int>();
 
-                foreach (ArticuloEN art in usuario[0].Articulo) {
+                foreach (ArticuloEN art in usuario[0].Articulo)
+                {
                     aux.Add(art.Id);
                 }
-                foreach (MetodoPagoEN metodo in usuario[0].MetodoPago) {
-                        usuarioPremiumEN.MetodoPago.Add (metodo);
+                foreach (MetodoPagoEN metodo in usuario[0].MetodoPago)
+                {
+                    usuarioPremiumEN.MetodoPago.Add(metodo);
                 }
 
-                oid = usuarioPremiumCEN.get_IUsuarioPremiumRepository ().CrearUsuarioPremium (usuarioPremiumEN);
+                oid = usuarioPremiumCEN.get_IUsuarioPremiumRepository().CrearUsuarioPremium(usuarioPremiumEN);
                 usuarioCEN.AnyadirArticulo(oid, aux);
 
-                result = usuarioPremiumCEN.get_IUsuarioPremiumRepository ().ReadOIDDefault (oid);
-                CPSession.Commit ();
-                usuarioCEN.BorrarUsuario (usuario[0].Id);
-        }
-        catch (Exception ex)
-        {
-                CPSession.RollBack ();
+                result = usuarioPremiumCEN.get_IUsuarioPremiumRepository().ReadOIDDefault(oid);
+                CPSession.Commit();
+                usuarioCEN.BorrarUsuario(usuario[0].Id);
+            }
+            catch (Exception ex)
+            {
+                CPSession.RollBack();
                 throw ex;
-        }
-        finally
-        {
-                CPSession.SessionClose ();
-        }
-        return result;
+            }
+            finally
+            {
+                CPSession.SessionClose();
+            }
+            return result;
 
-
-        /*PROTECTED REGION END*/
-}
-}
+            /*PROTECTED REGION END*/
+        }
+    }
 }
